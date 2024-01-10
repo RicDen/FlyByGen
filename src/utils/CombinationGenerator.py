99430a4dd348392b421c745d17bc8e6e1b865e1e
Ric <Ric>
2024-01-10 16:34:21 +0200

    increased core/addon number flexibility

--- src/utils/CombinationGenerator.py
+++ src/utils/CombinationGenerator.py
@@ -1,14 +1,15 @@
 from itertools import product
 
-def generate_object_values(parameter_ranges, increments):
+def generate_value_combination(parameter_ranges, increments):
     # Get the parameter names (e.g., 'radius', 'length')
     object_names = list(parameter_ranges.keys())
     # Create a list to store all combinations
-    object_values = {}
+    object_value_combos = {}
 
     # Create combinations for all parameters of each object
 
     for obj_name in object_names:
+        value_combinations = []
         parameter_names = list(parameter_ranges[obj_name].keys())
         parameter_combinations = product(*[generate_values(parameter_ranges[obj_name][param]['min'],
                                                            parameter_ranges[obj_name][param]['max'],
@@ -18,26 +19,31 @@ def generate_object_values(parameter_ranges, increments):
             object_value = {}
             for param, value in zip(parameter_names, param_values):
                 object_value[param] = value
-            index_obj_name = f"{obj_name}_{index}"
-            object_values[index_obj_name] = object_value
+            # index_obj_name = f"{obj_name}_{index}"
+            value_combinations.append(object_value)
+        object_value_combos[obj_name] = value_combinations
+    return object_value_combos
 
-    # Create combinations of cores with addons
-    core_combinations = [key for key in object_values.keys() if key.startswith('core')]
-    addon_combinations = [key for key in object_values.keys() if key.startswith('addon')]
+def generate_object_combinations(object_values):
+    permutations = list(product(*object_values.values()))
+    return [dict(zip(object_values.keys(), p)) for p in permutations]
 
-    combined_values = {}
-    for core_comb in core_combinations:
-        for addon_comb in addon_combinations:
 
-            combined_key = f"{core_comb}, {addon_comb}"
-            core_values = object_values[core_comb]
-            addon_values = object_values[addon_comb]
-            combined_value = {}
-            combined_value[core_comb] = core_values
-            combined_value[addon_comb] = addon_values
-            combined_values[combined_key] = combined_value
-
-    return combined_values
+    #
+    # for obj_name, values in object_values.items():
+    #     combined_objects = {}
+    #     for core_comb in core_combinations:
+    #         for addon_comb in addon_combinations:
+    #
+    #             combined_key = f"{core_comb}, {addon_comb}"
+    #             core_values = object_value_combinations[core_comb]
+    #             addon_values = object_value_combinations[addon_comb]
+    #             combined_value = {}
+    #             combined_value[core_comb] = core_values
+    #             combined_value[addon_comb] = addon_values
+    #             combined_objects[combined_key] = combined_value
+    #
+    # return combined_objects
 
 # Helper function to generate floating-point values
 def generate_values(min_value, max_value, increment):
@@ -46,22 +52,64 @@ def generate_values(min_value, max_value, increment):
         yield current_value
         current_value += increment
 
-# Example usage:
-parameter_ranges = {
+# Example usage: Normal int/float values for nucleus generator:
+parameter_ranges_nucleus = {
     'core': {'radius': {'min': 1.0, 'max': 3.0}, 'length': {'min': 1.0, 'max': 2.0}},
     'addon1': {'radius': {'min': 1.5, 'max':2.5}, 'length': {'min': 1.0, 'max': 3.0}},
+    'addon2': {'radius': {'min': 1.5, 'max':2.5}, 'length': {'min': 1.0, 'max': 3.0}}
 }
 
-increments = {'core': {'radius': 1.0, 'length': 1.0}, 'addon1': {'radius': 1.0, 'length': 1.0}}
+increments_nucleus = {
+                'core': {'radius': 1.0, 'length': 1.0},
+                'addon1': {'radius': 1.0, 'length': 1.0},
+                'addon2': {'radius': 1.0, 'length': 1.0}
+              }
 
-#
-# parameter_ranges = {
-#     'core': {'radius': {'min': 1.0, 'max': 3.0}, 'length': {'min': 1.0, 'max': 3.0}, 'rotation': [{'min': 1.0, 'max': 3.0},{'min': 2.0, 'max': 3.0}, {'min': 2.0, 'max': 4.0}]},
-#     'addon1': {'radius': {'min': 1.5, 'max': 4.5}, 'length': {'min': 1.0, 'max': 3.0}},
-# }
-#
-# increments = {'core': {'radius': 1.0, 'length': 1.0, 'rotation':[1,1,1]}, 'addon1': {'radius': 1.0, 'length': 1.0}}
+print("Simple nucleus parameter range testing:")
+value_combinations_nucleus = generate_value_combination(parameter_ranges_nucleus, increments_nucleus)
+print("Combination of each object:")
+for combination in value_combinations_nucleus:
+    print(f"{combination}: {value_combinations_nucleus[combination]}")
+combinations_nucleus = generate_object_combinations(value_combinations_nucleus)
+print("Combination of all objects within the range:")
+for combination in combinations_nucleus:
+    print(f"{combination}")
 
-combinations = generate_object_values(parameter_ranges, increments)
-for combination in combinations:
-    print(f"{combination}: {combinations[combination]}")
+# # Example usage 2:  Normal values with other levels of nexted than core, for example animation:
+# parameter_ranges_animation ={
+#         "rotation_parameters": {
+#         "B2_main_rotation": {
+#             "period": {'min': 3, 'max': 5}
+#         },
+#         "B2_precession": {
+#             "period": {'min': 90, 'max': 110}
+#         }
+#     },
+#         "fly_by_parameters": {
+#         "speed": {'min': 75, 'max': 85},
+#         "t_start": {'min': -1230, 'max': -1110}
+#         },
+#         "fps": {'min': 23, 'max': 25}
+#     }
+#
+# increments_animation ={
+#         "rotation_parameters": {
+#         "B2_main_rotation": {
+#             "period": 1
+#         },
+#         "B2_precession": {
+#             "period": 10
+#         }
+#     },
+#         "fly_by_parameters": {
+#         "speed": 5,
+#         "t_start": 10
+#         },
+#         "fps": 1
+#     }
+#
+# # print("Animation parameter range testing:")
+# # value_combinations_animation = generate_value_combination(parameter_ranges_animation, increments_animation)
+# # # combinations_animation = generate_object_combinations(value_combinations_animation)
+# # for combination in value_combinations_animation:
+# #     print(f"{combination}: {value_combinations_animation[combination]}")
