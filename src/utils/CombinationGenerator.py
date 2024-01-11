ba5758889bd06fd75d630ec65001b105b0d5543c
Ric <Ric>
2024-01-11 22:23:10 +0200

    Combination Generator working full, but not cleaned or commented

--- src/utils/CombinationGenerator.py
+++ src/utils/CombinationGenerator.py
@@ -1,47 +1,24 @@
-from itertools import product
+from itertools import product, combinations
 
 
 def generate_value_combo_list(params, increments):
     generated_values = []
     for param in params:
-        if isinstance(params[param], dict):
+        if isinstance(increments[param], float) or isinstance(increments[param], int):
             generated_values.append(generate_values_from_range(params[param], increments[param]))
         elif isinstance(params[param], list):
             print("Vector in form of list provided")
             vector = []
             for (i, value) in enumerate(params[param]):
                 vector.append(generate_values_from_range(value, increments[param][i]))
-            # permutations = list(product(*vector))
             permutations = [list(perm) for perm in product(*vector)]
             generated_values.append(permutations)
             # and created all list option
         else:
             print("Error: Invalid parameters")
-    # parameter_combinations = product(
-    #     *[generate_values(params[param], increments[obj_name][param]) for param in params])
     return product(*generated_values)
 
 def assign_value_names(parameter_combinations, params):
-    """
-        Generates a list of dictionaries containing the combination of parameters value with their name
-        Parameters:
-        - parameter_combinations (list): List of parameter combinations obtained from itertools.product.
-        - params (list): List of parameter names.
-
-        Returns:
-        - list: List of dictionaries representing value combinations for each parameter set.
-
-        Example:
-        ```python
-        parameter_combinations = [(1.0, 2.0), (3.0, 4.0)]
-        params = ['radius', 'length']
-        result = generate_values_combinations(parameter_combinations, params)
-        ```
-        Result:
-        ```python
-        [{'radius': 1.0, 'length': 2.0}, {'radius': 3.0, 'length': 4.0}]
-        ```
-    """
     value_combinations = []
 
     # Iterate over each parameter combination
@@ -64,47 +41,62 @@ def generate_config_combinations(object_values):
     return permutations
 
 def generate_parameter_combinations(parameter_ranges, increments):
-    object_value_combo_dict = {}
+    combo_dict = {}
+    first_layer_entries = {}
+    first_layer_entries["range"] = {}
+    first_layer_entries["increments"] = {}
     recursion = False
+    parameter = False
     for key, value in increments.items():
         if isinstance(value, dict):
-            print("Going deeper")
             recursion = True
-            object_value_combo_dict[key] = generate_parameter_combinations(parameter_ranges[key], increments[key])
-        # else:
+            combo_dict[key] = generate_parameter_combinations(parameter_ranges[key], increments[key])
+        else:
+            parameter = True
+            first_layer_entries["range"][key] = parameter_ranges[key]
+            first_layer_entries["increments"][key] = increments[key]
 
+    if recursion:
+        recursive_list = generate_config_combinations(combo_dict)
 
-            # generate_parameter_combinations(parameter_ranges[key], increments[key])
-    if (recursion):
-        return generate_config_combinations(object_value_combo_dict)
-    else:
-        parameter_value_combinations =  generate_value_combo_list(parameter_ranges, increments)
-        object_value_combo_dict = assign_value_names(parameter_value_combinations, parameter_ranges)
-        return  object_value_combo_dict
+    if parameter:
+        if parameter_ranges != first_layer_entries["range"]:
+            print("Creating first layer combos")
+            first_layer_parameter_value_combinations = generate_value_combo_list(first_layer_entries["range"], first_layer_entries["increments"])
+            first_layer_combo_list = assign_value_names(first_layer_parameter_value_combinations, first_layer_entries["range"])
+            recursive_list = [{**dict1, **dict2} for dict1, dict2 in product(first_layer_combo_list, recursive_list)]
 
+        if len(combo_dict) == 0:
+            recursive_parameter_value_combinations = generate_value_combo_list(parameter_ranges, increments)
+            recursive_list = assign_value_names(recursive_parameter_value_combinations, parameter_ranges)
+
+    return recursive_list
 # Helper function to generate values
 def generate_values_from_range(param, increment):
-    min_value = param.get('min', 0)  # Default to 0 if 'min' is not present
-    max_value = param.get('max', 0)  # Default to 0 if 'max' is not present
+    min_value = param.get('min', 0)
+    max_value = param.get('max', 0)
     current_value = min_value
     values = []
     while current_value <= max_value:
         values.append(current_value)
-        current_value += increment
+        if isinstance(current_value,dict):
+            break;
+        else:
+            current_value += increment
     return values
 
 
 
-#
+
 # # Example usage 1: Normal int/float values for nucleus generator:
 # parameter_ranges_nucleus = {
-#     'core': {'radius': {'min': 1.0, 'max': 3.0}},
-#     'addon1': {'radius': {'min': 1.5, 'max':2.5}, 'length': {'min': 1.0, 'max': 3.0}}
+#     'core': {'radius': {'min': 1.5, 'max':2.5}, 'length': {'min': 1.0, 'max': 3.0}},
+#     'addon1': {'min': 1.5, 'max': 2.5}
 # }
 #
 # increments_nucleus = {
-#     'core': {'radius': 1.0},
-#     'addon1': {'radius': 1.0, 'length': 1.0}
+#     'core': {'radius': 1.0, 'length': 1.0},
+#     'addon1': 1.0
 # }
 #
 # print("Simple nucleus parameter range testing:")
@@ -134,14 +126,10 @@ def generate_values_from_range(param, increment):
 
 # Example usage 3:  Normal values with other levels of nexted than core, for example animation:
 parameter_ranges_animation ={
-    "fps": {'min': 23, 'max': 25},
-    "2fps": {'min': 20, 'max': 23},
+    "fps": {'min': 22, 'max': 25},
     "rotation_parameters": {
         "B2_main_rotation": {
             "period": {'min': 3, 'max': 5}
-        },
-        "B2_precession": {
-            "period": {'min': 90, 'max': 110}
         }
     },
     "fly_by_parameters": {
@@ -152,13 +140,9 @@ parameter_ranges_animation ={
 
 increments_animation ={
     "fps": 1,
-    "2fps": 1,
     "rotation_parameters": {
         "B2_main_rotation": {
             "period": 1
-        },
-        "B2_precession": {
-            "period": 10
         }
     },
     "fly_by_parameters": {
